efc0c9802800bc21d690b0f183bf7846c8bbc83e
Nilashish Roy <nilashishroyjoy@gmail.com>
2021-05-29 08:30:00 +0600

    Added Resource

---
+++ app/src/main/java/com/blackdiamond/apps/studio/vpn/widget/EmptyRecyclerView.java
@@ -0,0 +1,55 @@
+package com.blackdiamond.apps.studio.vpn.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+
+public class EmptyRecyclerView extends RecyclerView {
+
+    private View mEmptyView;
+
+    private AdapterDataObserver mDataObserver = new AdapterDataObserver() {
+        @Override
+        public void onChanged() {
+            super.onChanged();
+            updateEmptyView();
+        }
+    };
+
+    public EmptyRecyclerView(Context context) {
+        super(context);
+    }
+
+    public EmptyRecyclerView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public EmptyRecyclerView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+
+
+    @Override
+    public void setAdapter(RecyclerView.Adapter adapter) {
+        if (getAdapter() != null) {
+            getAdapter().unregisterAdapterDataObserver(mDataObserver);
+        }
+        if (adapter != null) {
+            adapter.registerAdapterDataObserver(mDataObserver);
+        }
+        super.setAdapter(adapter);
+        //updateEmptyView();
+    }
+
+    private void updateEmptyView() {
+        if (mEmptyView != null && getAdapter() != null) {
+            boolean showEmptyView = getAdapter().getItemCount() == 0;
+            mEmptyView.setVisibility(showEmptyView ? VISIBLE : GONE);
+            setVisibility(showEmptyView ? GONE : VISIBLE);
+        }
+    }
+}
